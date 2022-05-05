812dbcac4e833adeca281bcfb9d3fd7e08a08b93
Aaron Nguyen <57938293+Hoshikara@users.noreply.github.com>
2022-05-04 17:45:42 -0700

    ddf80287be8c4e99feb6117a2bdee8cf645bb431

--- Main/src/Application.cpp
+++ Main/src/Application.cpp
@@ -2392,7 +2392,7 @@ int lLoadSharedSkinTexture(lua_State* L) {
 	path = Path::Absolute(path);
 
 	newTexture->nvgTexture = nvgCreateImage(g_guiState.vg, path.c_str(), imageflags);
-	newTexture->texture = g_application->LoadTexture(path, true);
+	newTexture->texture = g_application->LoadTexture(filename, false);
 
 	if (newTexture->Valid())
 	{
