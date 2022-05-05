a3102721679a1c477986810f792edadfadcdb91e
Aaron Nguyen <57938293+Hoshikara@users.noreply.github.com>
2022-05-04 17:48:33 -0700

    0d5f7935430ddc54691762b328b1544e07ad4954

--- Main/src/SettingsScreen.cpp
+++ Main/src/SettingsScreen.cpp
@@ -343,6 +343,7 @@ private:
 			break;
 		case InputDevice::Mouse:
 			laserSensKey = GameConfigKeys::Mouse_Sensitivity;
+            Label(Utility::Sprintf("Estimated PPR: %.0f", Input::EstimatePprFromSens(g_gameConfig.GetFloat(laserSensKey))));
 			break;
 		case InputDevice::Keyboard:
 		default:
@@ -1339,6 +1340,7 @@ private:
 	float m_delta = 0.f;
 	float m_currentSetting = 0.f;
 	bool m_firstStart = false;
+    MouseLockHandle m_mouseLock;
 public:
 	LaserSensCalibrationScreen_Impl()
 	{
@@ -1355,9 +1357,14 @@ public:
 		g_input.GetInputLaserDir(0); //poll because there might be something idk
 
 		if (g_gameConfig.GetEnum<Enum_InputDevice>(GameConfigKeys::LaserInputDevice) == InputDevice::Controller)
-			m_currentSetting = g_gameConfig.GetFloat(GameConfigKeys::Controller_Sensitivity);
+        {
+            m_currentSetting = g_gameConfig.GetFloat(GameConfigKeys::Controller_Sensitivity);
+        }
 		else
+        {
 			m_currentSetting = g_gameConfig.GetFloat(GameConfigKeys::Mouse_Sensitivity);
+            m_mouseLock = g_input.LockMouse();
+        }
 
 		g_input.OnButtonPressed.Add(this, &LaserSensCalibrationScreen_Impl::OnButtonPressed);
 		return true;
@@ -1375,11 +1382,18 @@ public:
 
 		if (m_state)
 		{
-			const float sens = 6.0f / m_delta;
+			float sens = 6.0f / m_delta;
+			if (g_gameConfig.GetEnum<Enum_InputDevice>(GameConfigKeys::LaserInputDevice) == InputDevice::Mouse)
+			{
+				sens = Input::CalculateSensFromPpr(m_delta);
+			}
 
 			g_application->FastText("Turn left knob one revolution clockwise", center.x, center.y, 40, NVGalign::NVG_ALIGN_CENTER | NVGalign::NVG_ALIGN_MIDDLE);
 			g_application->FastText("then press start.", center.x, center.y + 45, 40, NVGalign::NVG_ALIGN_CENTER | NVGalign::NVG_ALIGN_MIDDLE);
-			g_application->FastText(Utility::Sprintf("Current Sens: %.2f", sens), center.x, center.y + 90, 40, NVGalign::NVG_ALIGN_CENTER | NVGalign::NVG_ALIGN_MIDDLE);
+			if (g_gameConfig.GetEnum<Enum_InputDevice>(GameConfigKeys::LaserInputDevice) == InputDevice::Mouse)
+				g_application->FastText(Utility::Sprintf("Current Sens: %.2f, ppr: (%.0f)", sens, fabs(m_delta)), center.x, center.y + 90, 40, NVGalign::NVG_ALIGN_CENTER | NVGalign::NVG_ALIGN_MIDDLE);
+			else
+				g_application->FastText(Utility::Sprintf("Current Sens: %.2f", sens), center.x, center.y + 90, 40, NVGalign::NVG_ALIGN_CENTER | NVGalign::NVG_ALIGN_MIDDLE);	
 
 		}
 		else
@@ -1398,7 +1412,12 @@ public:
 				if (m_state)
 				{
 					// calc sens and then call delegate
-					SensSet.Call(6.0f / m_delta);
+					float sens = 6.0f / m_delta;
+					if (g_gameConfig.GetEnum<Enum_InputDevice>(GameConfigKeys::LaserInputDevice) == InputDevice::Mouse)
+					{
+						sens = Input::CalculateSensFromPpr(m_delta);
+					}
+					SensSet.Call(sens);
 					g_application->RemoveTickable(this);
 				}
 				else
